8d20d5339e912f5f644fff0ae6ee1dd120cb225c
João Lima <jpcruzlima13@gmail.com>
2026-03-26 08:57:21 +0000

    feat: Add a form with email validation.
    q

---
+++ scripts/index.js
@@ -0,0 +1,80 @@
+const inputEmail = document.querySelector("#inputEmail");
+const errorMessage = document.querySelector(".form-message-type_error");
+const form = document.querySelector("#card-form");
+const cardSuccess = document.querySelector(".card__success");
+const successEmail = cardSuccess.querySelector(".card__email-success");
+const cardSignUp = document.querySelector(".card");
+const dismissButton = document.querySelector("#dismiss-button")
+
+
+function showError(message) {
+
+      inputEmail.classList.add("form-input-type_error");
+        errorMessage.classList.add("form-message-error_active");
+        errorMessage.textContent = message;
+       
+}
+
+function hideError () {
+      inputEmail.classList.remove("form-input-type_error");
+        errorMessage.classList.remove("form-message-error_active");
+        errorMessage.textContent = "";
+}
+
+
+function getEmailErrorMessage () {
+    if(inputEmail.validity.valueMissing) {
+      return "This field is required"
+    } 
+
+    if (inputEmail.validity.typeMismatch) {
+        return "Enter a valid email address."
+    }
+
+    return "";
+}
+
+
+function validateInput () {
+   if(!inputEmail.validity.valid) {
+    showError(getEmailErrorMessage());
+    return false;
+   } 
+
+   hideError();
+   return true;
+}
+
+
+inputEmail.addEventListener("input", () => {
+   validateInput();
+})
+
+
+
+
+
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+
+        if (!validateInput()) {
+            return;
+    } 
+
+      successEmail.textContent = inputEmail.value;
+        cardSuccess.classList.remove("card-hidden");
+        cardSignUp.classList.add("card-hidden");
+      
+
+
+        
+    
+})
+
+dismissButton.addEventListener("click", () => {
+    cardSignUp.classList.remove("card-hidden");
+      cardSuccess.classList.add("card-hidden");
+      form.reset();
+      hideError();
+})
